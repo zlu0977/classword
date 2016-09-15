0698bf19ad46c2cb4332606cb49a17640a514146
Student 8 <Student 8@10.8.33.168>
2016-09-15 13:44:04 -0400

    hi

---
+++ period8java/src/introduction/ProceduralExample.java
@@ -0,0 +1,10 @@
+package introduction;
+
+public class ProceduralExample {
+
+	public static void main(String[] args) {
+		String[] names = {"Bob", "Jillian", "Obama"};
+		System.out.println("Hi, my name is " + names[0]);
+	}
+
+}
