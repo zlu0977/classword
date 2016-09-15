4105133b59fd879a26ce2389daed6d21bdbd0fa8
zlu0977 <zlu0977@bths.edu>
2016-09-15 13:46:01 -0400

    git

--- period8java/src/introduction/ProceduralExample.java
+++ period8java/src/introduction/ProceduralExample.java
@@ -4,6 +4,7 @@ public class ProceduralExample {
 
 	public static void main(String[] args) {
 		String[] names = {"Bob", "Jillian", "Obama"};
+		String[] years = {"Freshmen", "Sophomore", "Junior", "Senior"};
 		System.out.println("Hi, my name is " + names[0]);
 	}
 
