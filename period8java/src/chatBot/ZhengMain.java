ca1bfe1617cbbb2d82914209f43e5f40794843ca
Student 8 <Student 8@10.8.33.168>
2016-09-27 14:03:59 -0400

    blah

--- period8java/src/chatBot/ZhengMain.java
+++ period8java/src/chatBot/ZhengMain.java
@@ -11,7 +11,7 @@ public class ZhengMain {
 	static ZhengSchool school = new ZhengSchool();
 	
 	public static void main(String[] args) {
-
+		syso("" + findKeyword("hello i am not GOOD. hi good", "good", 0));
 	}
 	
 	public static String promptInput()
@@ -45,7 +45,8 @@ public class ZhengMain {
 		
 		searchString = searchString.trim().toLowerCase();
 		keyword = keyword.toLowerCase();
-		int psn = searchString.indexOf(keyword);
+		int psn = searchString.indexOf(keyword, startPosition);
+		
 		while(psn >= 0)
 		{
 			String before = " ";
@@ -57,13 +58,37 @@ public class ZhengMain {
 			if(psn + keyword.length() < searchString.length())
 				after = searchString.substring(psn + keyword.length(), psn + keyword.length() + 1);
 			
-			if(before.compareTo("a") < 0 && after.compareTo("a") < 0)
+			if(before.compareTo("a") < 0 && after.compareTo("a") < 0 && noNegations(searchString, psn))
 				return psn;
 			else
 				psn = searchString.indexOf(keyword, psn + 1);
 		}
 		
 		return -1;
+	}
+	
+	private static boolean noNegations(String searchString, int psn) {
+		String[] negationsList = {"no ", "not ", "never ", "n't "};
+		
+		for(int i = 0; i < negationsList.length; i ++)
+		{
+			int negationLength = negationsList[i].length();
+			if(psn - negationLength >= 0 && searchString.substring(psn - negationLength, psn).equals(negationsList[i]))
+				return false;
+		}
+		return true;
+		
+		/*if(psn - 3 >= 0 && searchString.substring(psn - 3, psn).equals("no "))
+			return true;
+		  if(psn - 4 >= 0 && searchString.substring(psn - 4, psn).equals("not "))
+			return true;
+		  if(psn - 6 >= 0 && searchString.substring(psn - 6, psn).equals("never "))
+			return true;
+		  if(psn - 4 >= 0 && searchString.substring(psn - 4, psn).equals("n't "))
+			return true;
+			
+		return false;*/
+		
 	}
 
 	public static void syso(String string)
